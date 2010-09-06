baf3ff34f62014db5cf33cdd2c64cd1b7c2ef1ba
Léon Keijser <keijser@stone-it.com>
2010-09-06 07:15:25 +0200

    Take routing type (NAT/DR/TUN) into consideration

--- server.py
+++ server.py
@@ -42,37 +42,47 @@ def readLines(filename):
 print "Building ACL from LVS table ..."
 accessList={}
 for line in readLines('/proc/net/ip_vs'):
-	if line.split()[0] == 'TCP' or line.split()[0] == 'UDP':
-		vIP = line.split()[1].split(':')[0]
-		vIPfst = vIP[0:2]
-		vIPscd = vIP[2:4]
-		vIPtrd = vIP[4:6]
-		vIPfth = vIP[6:8]
-		vIPport = hex2dec(line.split()[1].split(':')[1])
-		virtualIP = str(hex2dec(vIPfst)) + "." + str(hex2dec(vIPscd)) + "." + str(hex2dec(vIPtrd)) + "." + str(hex2dec(vIPfth))
-	if line.split()[0] == '->':
-		if len(str(line.split()[1])) == 13:
-			rsIP = line.split()[1].split(':')[0]
-			rsIPfst = rsIP[0:2]	# first octet
-			rsIPscd = rsIP[2:4]	# second octet
-			rsIPtrd = rsIP[4:6]	# third octet
-			rsIPfth = rsIP[6:8]	# fourth octet
-			rsIPport = hex2dec(line.split()[1].split(':')[1])
-			# compile RIP
-			realserverIP = str(hex2dec(rsIPfst)) + "." + str(hex2dec(rsIPscd)) + "." + str(hex2dec(rsIPtrd)) + "." + str(hex2dec(rsIPfth))
-			# build realserver list
-			realserverList = [virtualIP]
-			# add original weight to list
-			realserverList.append(line.split()[3])
-			# add current weight to list (same as original weight)
-			realserverList.append(line.split()[3])
-			# add virtual port to list
-			realserverList.append(vIPport)
-			# add real port to list
-			realserverList.append(rsIPport)
-			# add keypair (ip:weight)  to ACL
-			accessList[realserverIP] = realserverList
-			print "Adding " + str(realserverIP) + ":" + str(virtualIP) + ":" + str(accessList[realserverIP][0]) + ":" + str(accessList[realserverIP][1]) + ":" + str(vIPport) + ":" + str(rsIPport) + " to ACL as RIP:VIP:weightOrig:weightCurrent:vPort:rPort"
+    if line.split()[0] == 'TCP' or line.split()[0] == 'UDP':
+        vIP = line.split()[1].split(':')[0]
+        vIPfst = vIP[0:2]
+        vIPscd = vIP[2:4]
+        vIPtrd = vIP[4:6]
+        vIPfth = vIP[6:8]
+        vIPport = hex2dec(line.split()[1].split(':')[1])
+        virtualIP = str(hex2dec(vIPfst)) + "." + str(hex2dec(vIPscd)) + "." + str(hex2dec(vIPtrd)) + "." + str(hex2dec(vIPfth))
+    if line.split()[0] == '->':
+        if len(str(line.split()[1])) == 13:
+            rsIP = line.split()[1].split(':')[0]
+            rsIPfst = rsIP[0:2]	# first octet
+            rsIPscd = rsIP[2:4]	# second octet
+            rsIPtrd = rsIP[4:6]	# third octet
+            rsIPfth = rsIP[6:8]	# fourth octet
+            rsIPport = hex2dec(line.split()[1].split(':')[1])
+            # get routing type (nat/direct/tun)
+            rsRouteType = line.split()[2]
+            if rsRouteType == "Route":
+                rsRoute = "g"
+            elif rsRouteType == "Masq":
+                rsRoute = "m"
+            elif rsRouteType == "Tunnel":
+                rsRoute = "i"
+            # compile RIP
+            realserverIP = str(hex2dec(rsIPfst)) + "." + str(hex2dec(rsIPscd)) + "." + str(hex2dec(rsIPtrd)) + "." + str(hex2dec(rsIPfth))
+            # build realserver list
+            realserverList = [virtualIP]
+            # add original weight to list
+            realserverList.append(line.split()[3])
+            # add current weight to list (same as original weight)
+            realserverList.append(line.split()[3])
+            # add virtual port to list
+            realserverList.append(vIPport)
+            # add real port to list
+            realserverList.append(rsIPport)
+            # add routing type to list
+            realserverList.append(rsRoute)
+            # add keypair (ip:weight)  to ACL
+            accessList[realserverIP] = realserverList
+            print "Adding %s:%s:%s:%s:%s:%s to ACL as RIP:VIP:weightOrig:weightCurrent:vPort:rPort:rType" % (str(realserverIP),str(virtualIP),str(accessList[realserverIP][1]),str(vIPport),str(rsIPport),str(rsRouteType))
 
 # Test function pushLoad
 def pushLoad_function(LoadAvg, clientIP):
@@ -92,8 +102,8 @@ def pushLoad_function(LoadAvg, clientIP):
         # Only change weight if not already changed
         if not int(rsWeightCurrent) == int(rsWeightHalf):
             print "Changing realserver's weight to 50% of original"
-            print "Running /sbin/ipvsadm -e -t " + str(accessList[clientIP][0]) + ":" + str(accessList[clientIP][3]) + " -r " + str(clientIP) + ":" + str(accessList[clientIP][4]) + " -w " + str(rsWeightHalf)
-            cmd = "/sbin/ipvsadm -e -t " + str(accessList[clientIP][0]) + ":" + str(accessList[clientIP][3]) + " -r " + str(clientIP) + ":" + str(accessList[clientIP][4]) + " -w " + str(rsWeightHalf)
+            print "Running /sbin/ipvsadm -e -t " + str(accessList[clientIP][0]) + ":" + str(accessList[clientIP][3]) + " -r " + str(clientIP) + ":" + str(accessList[clientIP][4]) + " -w " + str(rsWeightHalf) + " -" + str(accessList[clientIP][5])
+            cmd = "/sbin/ipvsadm -e -t " + str(accessList[clientIP][0]) + ":" + str(accessList[clientIP][3]) + " -r " + str(clientIP) + ":" + str(accessList[clientIP][4]) + " -w " + str(rsWeightHalf) + " -" + str(accessList[clientIP][5])
             os.system(cmd)
             # Setting new weight in list
             accessList[clientIP][2] = rsWeightHalf
@@ -106,8 +116,8 @@ def pushLoad_function(LoadAvg, clientIP):
 	# Only change weight if not already changed
         if not int(rsWeightCurrent) == int(rsWeightQuart):
             print "Changing realserver's weight to 75% of original"
-            print "Running /sbin/ipvsadm -e -t " + str(accessList[clientIP][0]) + ":" + str(accessList[clientIP][3]) + " -r " + str(clientIP) + ":" + str(accessList[clientIP][4]) + " -w " + str(rsWeightQuart)
-            cmd = "/sbin/ipvsadm -e -t " + str(accessList[clientIP][0]) + ":" + str(accessList[clientIP][3]) + " -r " + str(clientIP) + ":" + str(accessList[clientIP][4]) + " -w " + str(rsWeightQuart)
+            print "Running /sbin/ipvsadm -e -t " + str(accessList[clientIP][0]) + ":" + str(accessList[clientIP][3]) + " -r " + str(clientIP) + ":" + str(accessList[clientIP][4]) + " -w " + str(rsWeightQuart) + " -" + str(accessList[clientIP][5])
+            cmd = "/sbin/ipvsadm -e -t " + str(accessList[clientIP][0]) + ":" + str(accessList[clientIP][3]) + " -r " + str(clientIP) + ":" + str(accessList[clientIP][4]) + " -w " + str(rsWeightQuart) + " -" + str(accessList[clientIP][5])
 	    os.system(cmd)
             # Setting new weight in list
             accessList[clientIP][2] = rsWeightQuart
